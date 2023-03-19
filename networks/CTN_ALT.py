39b5f34675926d39c5d70171b24560c0aab6be17
Nikhil Suresh <niksure24@g.ucla.edu>
2023-03-18 23:44:58 -0700

    updates

--- networks/CTN_ALT.py
+++ networks/CTN_ALT.py
@@ -1,37 +1,42 @@
 import torch.nn as nn
-import torch 
+import torch
+
+import constants
+
 
 class CTN_ALT(torch.nn.Module):
-    """
-    Basic logistic regression on 224x224x3 images.
-    """
-#print the size of theoutput
-    def __init__(self, batch_size):
+    """Alternate CNN + Transformer Architectureqq"""
+    def __init__(self, model_config):
         super().__init__()
-        self.num_heads = 5
-        self.conv = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
+        self.conv = nn.Conv2d(1,
+                              model_config["OUT_CHANNELS"],
+                              kernel_size=3,
+                              stride=1,
+                              padding=1)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.transformer = nn.Transformer(d_model=125, nhead=self.num_heads, num_encoder_layers=12, num_decoder_layers=12, batch_first=True)
-        self.num_classes = 4
+        self.transformer = nn.Transformer(
+            d_model=model_config["D_MODEL"],
+            nhead=model_config["N_HEADS"],
+            num_encoder_layers=model_config["TRANSFORMER_LAYERS"],
+            num_decoder_layers=model_config["TRANSFORMER_LAYERS"],
+            batch_first=True)
         self.flatten = nn.Flatten()
-        self.dropout = nn.Dropout(p=0.4)
-        self.fc1 = nn.Linear(1375, 64)  
-        self.fc2 = nn.Linear(64, self.num_classes)
+        self.dropout = nn.Dropout(p=model_config["FC_DROPOUT"])
+        self.fc1 = nn.Linear(model_config["HIDDEN_SIZE_1"],
+                             model_config["HIDDEN_SIZE_2"])
+        self.fc2 = nn.Linear(model_config["HIDDEN_SIZE_2"],
+                             constants.DATA["NUM_CLASSES"])
 
     def forward(self, inp):
-        # print(inp.shape)
-        x = inp.unsqueeze(1) # Add channel dimension
+        x = inp.unsqueeze(1)  # Add channel dimension
         x = self.conv(x.float())
         x = self.pool(x)
-        # print(x.shape)
         src = x[:, 0, :, :].float()
         tgt = x[:, 1, :, :].float()
-        #src and trt isnt equal to the d model 
-        x = self.transformer(src, tgt) 
+        x = self.transformer(src, tgt)
         x = self.flatten(x)
-        x = self.dropout(x) 
-        
-        x = self.fc1(x) 
+        x = self.dropout(x)
+        x = self.fc1(x)
         x = self.dropout(x)
         x = self.fc2(x)
         return x
